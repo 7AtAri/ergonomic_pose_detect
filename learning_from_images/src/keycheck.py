ea3b27f4d8aa45bc5da9131302ada3b8d261746a
7AtAri <7AtAri@users.noreply.github.com>
2024-01-20 23:04:48 +0100

    requirements

--- learning_from_images/src/keycheck.py
+++
@@ -1,7 +0,0 @@
-# only for windows and Linux
-
-import keyboard
-
-print("Press the key you want to use for the hotkey.")
-key = keyboard.read_key()
-print(f"The key code for the pressed key is: {key}")
